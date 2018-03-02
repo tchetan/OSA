5f740d729a7eea3b711c6a63b17e937c28324128
tanish.chetan <tanish.chetan@gmail.com>
2018-03-02 22:55:50 +0530

    POM

--- src/com/Test5.java
+++ src/com/Test5.java
@@ -1,13 +1,14 @@
+
 package com;
 
-public class Test5 {
+public class Test5{
+	  public String getPersonalLoan(){
+	        return "Personal Loan";
+	  };
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//
-		//
-		//
+	  public static void main(String args){
+	     System.out.println("Inside main in Java");
+	  }
 	}
 
-}
+	
